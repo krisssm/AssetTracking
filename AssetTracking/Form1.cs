97560fbc5ecb3fa885749d311602862b43a3f873
Kris <Kris@DESKTOP-CMKT5V8>
2024-10-29 09:51:38 +0200

    Add project files.

---
+++ AssetTracking/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssetTracking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // Initial factory
+            var factory = new FlurlTbClientFactory
+            {
+                Options = new ThingsboardNetFlurlOptions()
+                {
+                    BaseUrl = "http://localhost:8080",
+                    Username = "kristjanmaetaly1.2.3.4@gmail.com",
+                    Password = "KristjanjaKregor",
+                }
+            };
+
+            // Get the client
+            var authClient = factory.CreateAuthClient();
+            var userInfo = await authClient.GetCurrentUserAsync();
+            Console.WriteLine($"Hello {userInfo.Email}");
+        }
+
+        InitializeComponent();
+        }
+    }
+}
---
+++ ImportFromDatabase/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thingsboard.Net.Flurl.Options;
+using Thingsboard.Net.Flurl;
+
+namespace ImportFromDatabase
+{
+
+
+    public class Class1
+    {
+        // Initial factory
+        var factory = new FlurlTbClientFactory
+        {
+            Options = new ThingsboardNetFlurlOptions()
+            {
+                BaseUrl = "http://localhost:8080",
+                Username = "kristjanmaetaly1.2.3.4@gmail.com",
+                Password = "KristjanjaKregor",
+            }
+        };
+
+        // Get the client
+        var authClient = factory.CreateAuthClient();
+        var userInfo = await authClient.GetCurrentUserAsync();
+        Console.WriteLine($"Hello {userInfo.Email}");
+    }
+}
