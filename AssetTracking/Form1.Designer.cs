cadcd312a4ab0a8737a98a1cba60d61fd2d76a83
krisssm <kristjanmaetaly1.2.3.4@gmail.com>
2024-11-03 12:17:41 +0200

    Näitab ringi raadiusega umbmäärast kaugust gatewayst ning saab MAC aadressiga otsida

--- AssetTracking/Form1.Designer.cs
+++ AssetTracking/Form1.Designer.cs
@@ -28,21 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnFloorplan = new System.Windows.Forms.Button();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblValRSSI = new System.Windows.Forms.Label();
+            this.lblRSSI = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnFloorplan
+            // 
+            this.btnFloorplan.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnFloorplan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnFloorplan.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFloorplan.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFloorplan.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFloorplan.Location = new System.Drawing.Point(58, 68);
+            this.btnFloorplan.Name = "btnFloorplan";
+            this.btnFloorplan.Size = new System.Drawing.Size(135, 50);
+            this.btnFloorplan.TabIndex = 0;
+            this.btnFloorplan.Text = "Floor plan";
+            this.btnFloorplan.UseVisualStyleBackColor = false;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(41, 195);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(168, 20);
+            this.textSearch.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter asset";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSearch.Location = new System.Drawing.Point(58, 221);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(135, 50);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblValRSSI
+            // 
+            this.lblValRSSI.AutoSize = true;
+            this.lblValRSSI.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValRSSI.Location = new System.Drawing.Point(21, 295);
+            this.lblValRSSI.Name = "lblValRSSI";
+            this.lblValRSSI.Size = new System.Drawing.Size(140, 25);
+            this.lblValRSSI.TabIndex = 5;
+            this.lblValRSSI.Text = "Value of RSSI:";
+            // 
+            // lblRSSI
+            // 
+            this.lblRSSI.AutoSize = true;
+            this.lblRSSI.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRSSI.Location = new System.Drawing.Point(170, 295);
+            this.lblRSSI.Name = "lblRSSI";
+            this.lblRSSI.Size = new System.Drawing.Size(42, 25);
+            this.lblRSSI.TabIndex = 6;
+            this.lblRSSI.Text = "-53";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1142, 751);
+            this.ClientSize = new System.Drawing.Size(1012, 750);
+            this.Controls.Add(this.lblRSSI);
+            this.Controls.Add(this.lblValRSSI);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.btnFloorplan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFloorplan;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblValRSSI;
+        private System.Windows.Forms.Label lblRSSI;
     }
 }
 
--- AssetTracking/Form1.cs
+++ AssetTracking/Form1.cs
@@ -7,38 +7,143 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Drawing.Drawing2D;
 
 namespace AssetTracking
 {
     public partial class Form1 : Form
     {
+        private PictureBox pictureFloorplan;
+
+        string rssiValue = "-53";
+
         public Form1()
         {
 
-   
+
+            //using (var conn = new MKConnection("192.168.1.74", "admin", "kristjanjakregor"))
+            //{
+            //    conn.Open();
+            //    var cmd = conn.CreateCommand("iot bluetooth scanners advertisements print where address=D4:01:C3:6B:9F:8E");
+            //    var result = cmd.ExecuteReader();
+            //    foreach (var line in result)
+            //    {
+            //        Console.WriteLine(line);
+            //        Console.WriteLine("TTT");
+            //    }
+
+            //    var cmd1 = conn.CreateCommand("iot bluetooth scanners advertisements print where address=DC:2C:6E:73:A0:D4");
+            //    var result1 = cmd1.ExecuteReader();
+            //    foreach (var line1 in result1)
+            //        Console.WriteLine(line1);
+            //    Console.WriteLine("Tere");
+            //    conn.Close();
+            //}
+
+            this.Text = "Draw Empty Circle on PictureBox Example";
+            this.Size = new Size(400, 400);
+
+            // Create and configure the PictureBox
+            pictureFloorplan = new PictureBox
+            {
+                Location = new Point(270, 0), // Position of the PictureBox
+                Size = new Size(750, 750), // Size of the PictureBox
+                BorderStyle = BorderStyle.FixedSingle,
+                Image = Image.FromFile("C:\\Users\\Kris\\Desktop\\Plaan.png"), // Set the image path
+                SizeMode = PictureBoxSizeMode.StretchImage // Adjust the image to fit the PictureBox
+            };
+
+            // Subscribe to the Paint event of the PictureBox
+            pictureFloorplan.Paint += new PaintEventHandler(PictureFloorplan_Paint);
+
+            // Add the PictureBox to the form
+            this.Controls.Add(pictureFloorplan);
+
+            InitializeComponent();
+        }
+
+  
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        //private void btnFloorplan_Click(object sender, EventArgs e)
+        //{
+        //    if (pictureFloorplan1.Visible == false)
+        //    {
+        //        pictureFloorplan1.Visible = true;
+        //    }
+        //    else if (pictureFloorplan1.Visible == true)
+        //    {
+        //        pictureFloorplan1.Visible = false;
+        //    }
+        //}
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
             using (var conn = new MKConnection("192.168.1.74", "admin", "kristjanjakregor"))
             {
                 conn.Open();
-                var cmd = conn.CreateCommand("iot bluetooth scanners advertisements print where address=D4:01:C3:6B:9F:8E");
+ 
+
+                string assetName = textSearch.Text; // Your variable that holds the MAC address
+                var cmd = conn.CreateCommand(string.Format("iot bluetooth scanners advertisements print where address={0}", assetName));
+
                 var result = cmd.ExecuteReader();
+                string originalString = "";
                 foreach (var line in result)
+                { 
                     Console.WriteLine(line);
+                    originalString = line;
+ 
+                }
+                    
+                Console.WriteLine("Tere1");
+                conn.Close();
 
-                var cmd1 = conn.CreateCommand("iot bluetooth scanners advertisements print where address=DC:2C:6E:73:A0:D4");
-                var result1 = cmd1.ExecuteReader();
-                foreach (var line1 in result1)
-                    Console.WriteLine(line1);
 
+                // Regular expression to match rssi value
+                var match = Regex.Match(originalString, @"rssi=(.*?)=");
+
+                if (match.Success)
+                {
+                    rssiValue = match.Groups[1].Value;
+                    Console.WriteLine($"The value of rssi is: {rssiValue}");
+                    lblRSSI.Text = rssiValue;
+                    pictureFloorplan.Invalidate();
+                }
+                else
+                {
+                    Console.WriteLine("Rssi not found in the string.");
+                }
             }
+        }
+        private void PictureFloorplan_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the Graphics object from the PaintEventArgs
+            Graphics g = e.Graphics;
 
+            // Define the circle's position and size
+            int centerX = pictureFloorplan.Width / 2; // Center of the PictureBox
+            int centerY = pictureFloorplan.Height / 2; // Center of the PictureBox
+            int radius = (int.Parse(rssiValue) -20) * -3; // Radius of the circle
 
-
-
-            InitializeComponent();
+            // Create a pen to draw the circle
+            using (Pen pen = new Pen(Color.Red, 2)) // Color and width of the pen
+            {
+                // Draw the empty circle
+                g.DrawEllipse(pen, centerX - radius, centerY - radius, radius * 2, radius * 2);
+            }
         }
     }
 }
